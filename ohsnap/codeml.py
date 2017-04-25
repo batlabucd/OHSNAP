102f02403125d7f56dc45372967d8a1a3c9790ea
Michael Clarke <michael.clarke2.718@gmail.com>
2017-04-25 11:44:54 +0100

    reorganised package directory

--- ohsnap/codeml.py
+++ ohsnap/codeml.py
@@ -28,14 +28,17 @@ def cmp_anc_phy_names(species_tree, phy):
 			raise ValueError('Unknown phylip name "{0}"'.format(phy_name))
 			
 	
-def get_species_groups(branchlbl_dir):
+def get_species_groups(branchlbl_dir, blbl_ext='.txt'):
 	"""Return the species group names (basename no extension) for branch label files in 
 		branchlbl_dir."""
-	basenames = [ os.path.basename(f) if os.path.splitext(f)[1] == '.txt' else None for f in os.listdir(branchlbl_dir) ]
+	# get the .txt files in branchlbl_dir
+	basenames = [ os.path.basename(f) if os.path.splitext(f)[1] == blbl_ext else None for f in os.listdir(branchlbl_dir) ]
+	# make a list of the filenames without extension
 	species_groups = list(map(lambda f: os.path.splitext(f)[0], filter(None, basenames)))
 	return species_groups
 	
-def get_phy_to_group_mapping(branchlbl_dir, phy_dir, phy_ext='.phy'):
+	
+def get_phy_to_group_mapping(branchlbl_dir, phy_dir, phy_ext='.phy', blbl_ext='.txt'):
 	"""Return a dictionary of species groups to phy mappings, where only phy files that 
 		contain at least one species are mapped."""
 	map_list = []
@@ -50,7 +53,7 @@ def get_phy_to_group_mapping(branchlbl_dir, phy_dir, phy_ext='.phy'):
 	#check each branch label file and create a phy_name/group_name/phy_name mapping if 
 	#there's at least one species in the label file present in a phy file
 	for groupfile in os.listdir(branchlbl_dir):
-		if not os.path.splitext(groupfile)[1] == '.txt':
+		if not os.path.splitext(groupfile)[1] == blbl_ext:
 			continue
 		group_name = os.path.splitext(groupfile)[0]
 		with open(os.path.join(branchlbl_dir, groupfile)) as f:
@@ -61,12 +64,14 @@ def get_phy_to_group_mapping(branchlbl_dir, phy_dir, phy_ext='.phy'):
 						map_list.append(os.path.join(phy_name, group_name, phy_name))
 						break
 	return map_list
+	
 
 def get_output_files_phy_names(branchlbl_dir, mod_dir, phy_dir, labelled_models, \
-	mod_ext='.mod', phy_ext='.phy', out_ext='.out'):
+	mod_ext='.mod', phy_ext='.phy', out_ext='.out', blbl_ext='.txt'):
 	"""Return the control filenames for a set of models and phy alignment files. If a model 
 		requires a labelled branch (i.e. the model name is in the labelled_models list), a 
 		control file for each species group present in branchlbl_dir will be generated."""
+	# create and populate the lists of labelled an unlabelled models
 	lbl_mod_wcs = []
 	mod_wcs = []
 	for mod in os.listdir(mod_dir):
@@ -75,24 +80,30 @@ def get_output_files_phy_names(branchlbl_dir, mod_dir, phy_dir, labelled_models,
 			lbl_mod_wcs.append(name)
 		elif ext == mod_ext:
 			mod_wcs.append(name)
+	# get the list of phy file names (no extension)
 	phy_wcs = [ os.path.splitext(f)[0] for f in os.listdir(phy_dir) if os.path.splitext(f)[1] == phy_ext ]
-	#species_groups = get_species_groups(branchlbl_dir)
-	#return expand('{mod}/{phy}/{group}/{phy}.{mod}{ext}', mod=lbl_mod_wcs, phy=phy_wcs, group=species_groups, ext=out_ext) + \
-	#	expand('{mod}/{phy}/{phy}.{mod}{ext}', mod=mod_wcs, phy=phy_wcs, ext=out_ext)
+	# get the phy file name to group mapping, where only phy files that contain at least one species are 
+	# present in the map
 	phy_group_map = get_phy_to_group_mapping(branchlbl_dir, phy_dir, phy_ext)
+	# return the list of output filenames for each model and each phy file, for both labelled 
+	# unlabelled models
 	return expand('{mod}/{phy_group_map}.{mod}{ext}', mod=lbl_mod_wcs, phy_group_map=phy_group_map, ext=out_ext) + \
 		expand('{mod}/{phy}/{phy}.{mod}{ext}', mod=mod_wcs, phy=phy_wcs, ext=out_ext)
+		
 	
 def label_species_tree(input_tree, species_list, output_tree, label='#1', allow_not_found=False):
 	"""Label a clade in a species tree that is the common ancestor of all species in 
 		species_list. If allow_not_found is False, a ValueError will be raised if a species 
 		in species_list is not found in the input_tree."""
+	# read the list of species to label
 	species = []
 	with open(species_list) as f:
 		species = f.read().strip().split(',')
+	# read the tree file
 	tree = None
 	with open(input_tree) as f:
 		tree = next(Phylo.parse(f, 'newick'))
+	# find the clades in the tree that contain each species
 	clades = []
 	not_found = []
 	for sp in species:
@@ -101,27 +112,35 @@ def label_species_tree(input_tree, species_list, output_tree, label='#1', allow_
 			clades.append(clade)
 		except StopIteration:
 			not_found.append(sp)
+	# raise a ValueError if there were species not found and allow_not_found is False or
+	# raise a ValueError if no species were found
 	if len(not_found) > 0 and not allow_not_found:
 		raise ValueError('Cound not find species {0}'.format(not_found))
 	elif len(clades) == 0:
 		raise ValueError('No species in species_tree found to label.')
+	# get the clade common to all clades found
 	common_anc_clade = tree.common_ancestor(*clades)
 	if not common_anc_clade.name is None:
 		common_anc_clade = tree.get_path(common_anc_clade)[-2]
-	common_anc_clade.name = '#1'
+	common_anc_clade.name = label
 	with open(output_tree, 'w') as o:
 		Phylo.write([tree], o, 'newick', plain=True)
+		
 
 def phy_parser(phy):
 	"""Parse a phylip formatted file returning a list of tuples of seqname and alignment."""
 	num_records = None
 	align_length = None
 	with open(phy) as f:
+		# the first line in the file contains the number of alignments and the length of 
+		# each alignment
 		num_records, align_length = list(map(int, filter(None, f.readline().split(' '))))
 		records = []
 		for i in range(0, num_records):
 			seqname = f.readline().strip()
 			alignment = ''
+			# alignment can either start two spaces after the sequence name or on the 
+			# next line
 			parts = list(filter(None, seqname.split('  ')))
 			if len(parts) > 1:
 				seqname, alignment = list(map(lambda x: x.strip(), parts))
@@ -129,23 +148,31 @@ def phy_parser(phy):
 				alignment += f.readline().strip()
 				if len(alignment) > align_length:
 					break
+			# make sure the alignment length is the same as the length given at the start 
+			# of the file and raise a ValueError if it is not
 			if not len(alignment) == align_length:
 				raise ValueError(\
 					'Sequence name {0}: alignment length is not the expected length ({1})'.format(\
 						seqname, align_length))
 			records.append((seqname, alignment))
+	# make sure the number of records parsed was the same as the number given at the start 
+	# of the file
 	if not num_records == len(records):
 		raise ValueError(\
 			'Number of records read ({0}) does not match the number of expected records ({1})'.format(\
 				num_records, len(records)))
 	return records
 	
+	
 def prune_species_tree(input_tree, phy, output_tree):
 	"""Remove species from input_tree that are not in phy."""
+	# get the list of sequence names (named after species)
 	phy_names = list(map(lambda x: x[0], phy_parser(phy)))
+	# parse the species tree
 	tree = None
 	with open(input_tree) as f:
 		tree = next(Phylo.parse(f, 'newick'))
+	# loop through each named clade and make sure it is present in the list of sequence names
 	for element in tree.find_elements():
 		if not isinstance(element, Clade):
 			continue
@@ -154,8 +181,10 @@ def prune_species_tree(input_tree, phy, output_tree):
 	with open(output_tree, 'w') as o:
 		Phylo.write([tree], o, 'newick', plain=True)
 		
+		
 def write_control_file(input, output, wildcards):
-	"""Generate a CodeML control file."""
+	"""Generate a CodeML control file from a template and replace variable placeholder 
+		with values from Snakemake wildcards."""
 	namespace = {}
 	namespace.update(vars(wildcards))
 	namespace['species_tree_fn'] = os.path.abspath(input.species_tree)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='OHSNAP',
+	version='1.0',
+	description='',
+	author='Michael Clarke',
+	author_email='michael.clarke@ucd.ie',
+	url='https://github.com/batlabucd/OHSNAP',
+	packages=['ohsnap']
+)
+
